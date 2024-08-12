b2098a3af24b289c7df805990a74c78e546b4beb
Kevin Reaño Cisneros <kcisneros@moventicorp.com>
2024-08-11 19:44:56 -0500

    Add PermissionType.cs and Permission.cs to Application/Models

---
+++ N5Challenge.Application/Models/Permission.cs
@@ -0,0 +1,12 @@
+﻿namespace N5Challenge.Application.Models
+{
+    public class Permission
+    {
+        public int Id { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeLastName { get; set; }
+        public int PermissionTypeId { get; set; }
+        public DateTime PermissionDate { get; set; }
+        public PermissionType PermissionType { get; set; }
+    }
+}
